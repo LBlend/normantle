d5be4131efc2876252a86fee516b2ee9ee8d631f
LBlend <lblend@protonmail.com>
2022-05-24 19:59:57 +0200

    create API models

---
+++ models.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+class TodayInfo(BaseModel):
+    puzzleNumber: int
+    similarity: int
+    similarityTenth: float
+    similarityThousandth: float
+
+
+class Guess(BaseModel):
+    word: str
+    puzzleNumber: int
+
+
+class GuessReult(BaseModel):
+    word: str
+    similarity: float
+    isClose: bool
+    isCorrect: bool
+
+
+class Hint(BaseModel):
+    puzzleNumber: int
+    bestGuess: str
