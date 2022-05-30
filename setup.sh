cf3c7e6c614aa105fe8b7b2e3798344d6cd08c91
LBlend <lblend@protonmail.com>
2022-05-31 00:16:54 +0200

    create setup script with prefiltered model

--- setup.sh
+++ setup.sh
@@ -4,13 +4,7 @@ echo "Installing Python dependencies..."
 python3 -m pip install -r requirements.txt -U
 
 if [ ! -d 'model' ]; then
-    echo "Downloading word embeddings..."
-    wget http://vectors.nlpl.eu/repository/20/93.zip
-    unzip 93.zip -d model
-    rm 93.zip
-fi
-
-if [ ! -f 'model/new_model.txt' ]; then
-    echo "Filtering model..."
-    python3 scripts/filter_model.py
+    wget https://lblend.moe/dl/normantle/93_filtered.zip
+    unzip 93_filtered.zip -d model
+    rm 93_filtered.zip
 fi
