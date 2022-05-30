a69a31d0cee2f82388fae9efb2e9740204cb9a98
LBlend <lblend@protonmail.com>
2022-05-30 20:41:59 +0200

    change source model

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ python3 -m pip install -r requirements.txt -U
 
 if [ ! -d 'model' ]; then
     echo "Downloading word embeddings..."
-    wget http://vectors.nlpl.eu/repository/20/76.zip
-    unzip 76.zip -d model
-    rm 76.zip
+    wget http://vectors.nlpl.eu/repository/20/93.zip
+    unzip 93.zip -d model
+    rm 93.zip
 fi
 
 if [ ! -f 'model/new_model.txt' ]; then
