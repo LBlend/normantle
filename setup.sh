a116babe5f4e8ad9e79287d493c6173016ab64fa
Leander Furumo <leander@furumo.eu>
2024-12-16 19:08:02 +0100

    change pre-filtered model download link

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Installing Python dependencies..."
 python3 -m pip install -r requirements.txt -U
 
 if [ ! -d 'model' ]; then
-    wget https://lblend.moe/dl/normantle/93_filtered.zip
-    unzip 93_filtered.zip -d model
-    rm 93_filtered.zip
+  wget https://dl.furumo.eu/93_filtered.zip
+  unzip 93_filtered.zip -d model
+  rm 93_filtered.zip
 fi
